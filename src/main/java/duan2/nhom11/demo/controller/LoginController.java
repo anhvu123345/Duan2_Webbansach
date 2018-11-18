e84644568e94696a3572740418d9de6216a55bad
Trituee12 <nguyentuanngai@gmail.com>
2018-11-18 20:31:39 +0700

    sua LoginController,html, hien thi ten ng dung dang nhap, check session

--- src/main/java/duan2/nhom11/demo/controller/LoginController.java
+++ src/main/java/duan2/nhom11/demo/controller/LoginController.java
@@ -13,28 +13,35 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class LoginController {
 
-	@GetMapping(value="/login")
-	public ModelAndView login(String error, String logout) {
+	@GetMapping(value = "/login")
+	public ModelAndView login(String error, String logout, HttpServletRequest ck) {
 		ModelAndView model = new ModelAndView();
-		
-		if(error !=null) {
-			model.addObject("error", "Bạn nhập sai email hoặc mật khẩu");
+
+		if (ck.getUserPrincipal() != null) {
+			model.setViewName("redirect:/welcome");
+			return model;
+		} else {
+			if (error != null) {
+				model.addObject("error", "Bạn nhập sai email hoặc mật khẩu");
+
+			}
+			if (logout != null) {
+				model.addObject("logout", "Bạn đã đăng xuất thành công");
+
+			}
+			model.setViewName("web/login");
+			return model;
 		}
-		if(logout !=null) {
-			model.addObject("logout", "Bạn đã đăng xuất thành công");
-		}
-		model.setViewName("web/login");
-		return model;
 	}
-	
-	@GetMapping(value="logout")
-	 public String logout(HttpServletRequest request, HttpServletResponse response) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication != null) {
-            new SecurityContextLogoutHandler().logout(request, response, authentication);
-        }
-        
-        return "redirect:/login";
-    
+
+	@GetMapping(value = "logout")
+	public String logout(HttpServletRequest request, HttpServletResponse response) {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if (authentication != null) {
+			new SecurityContextLogoutHandler().logout(request, response, authentication);
+		}
+
+		return "redirect:/login";
+
 	}
 }
