fd9d74a040313607de24ef28aca6b61f78ace2d3
Trituee12 <nguyentuanngai@gmail.com>
2018-11-18 20:23:15 +0700

    cap nhap WebSecurityConfig.java (phan quyen, remember me)

--- src/main/java/duan2/nhom11/demo/config/WebSecurityConfig.java
+++ src/main/java/duan2/nhom11/demo/config/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package duan2.nhom11.demo.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +10,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
 import duan2.nhom11.demo.service.UserDetailsServiceImpl;
 
@@ -16,6 +20,9 @@ import duan2.nhom11.demo.service.UserDetailsServiceImpl;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private UserDetailsServiceImpl userDetailsServiceImpl;
+	
+	@Autowired
+	private DataSource dataSource;
 
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -24,26 +31,34 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-			http.authorizeRequests()
-		.antMatchers("/login").permitAll()
-		.antMatchers("/user/registrantion","/user/sign").permitAll()
-		
-					
-		.and()
-		.formLogin()
-		.loginPage("/login")
-		.defaultSuccessUrl("/welcome")		
-		.usernameParameter("email")
-		.passwordParameter("password")
-		.and()
-		.logout().permitAll()
-		.and()
-		.exceptionHandling().accessDeniedPage("/403");
-		
+		http.authorizeRequests().antMatchers("/", "/login", "/logout").permitAll();
+		http.authorizeRequests()
+				.antMatchers("/welcome", "/manager/catagory/list", "/manager/product/list", "/manager/catagory/add",
+							 "/manager/product/add", "/manager/{?}/upload", "/manager/product/{?}/edit")
+				.access("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')");
+		http.authorizeRequests().antMatchers("/admin/userlist").access("hasRole('ROLE_ADMIN')");
+		http.authorizeRequests().and().exceptionHandling().accessDeniedPage("/login");
+		http.authorizeRequests().and().formLogin()//
+				// Submit URL của trang login
+				.loginProcessingUrl("/checklogin") // Submit URL
+				.loginPage("/login").defaultSuccessUrl("/welcome").failureUrl("/login?error=true")//
+				.usernameParameter("email")//
+				.passwordParameter("password").and().logout().logoutUrl("/logout")
+				.logoutSuccessUrl("/logoutSuccessful");
+		//
+		http.rememberMe().key("uniqueAndSecret").tokenValiditySeconds(1296000);
+
 	}
 
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsServiceImpl).passwordEncoder(bCryptPasswordEncoder());
+	}
+
+	@Bean
+	public PersistentTokenRepository persistentTokenRepository() {
+		JdbcTokenRepositoryImpl db = new JdbcTokenRepositoryImpl();
+		db.setDataSource(dataSource);
+		return db;
 	}
 }
